0fc1939f4e6a203e610f8729a8ff1e900817430a
Brian Vo <brianhvo02@gmail.com>
2023-05-08 17:49:19 -0700

    Rewrite tmdb

--- backend/src/routes/tmdb.js
+++ backend/src/routes/tmdb.js
@@ -5,42 +5,40 @@ import { snakeToCamel } from '../utils';
 const router = Router();
 
 const fetchTMDB = (route, params) => fetch(`https://api.themoviedb.org/3${route}?api_key=${tmdbAPIKey}&${params}`).then(res => res.json());
-const allowedParams = ["id", "title", "overview", "poster_path", "backdrop_path", "genre_ids"]
+const allowedParams = ["id", "title", "overview", "poster_path", "backdrop_path", "genre_ids"];
+const extractAllowedParams = result => Object.fromEntries(allowedParams.map(key => [snakeToCamel(key), result[key]]))
 
 router.get('/genres', async (req, res) => {
-    const data = await fetchTMDB('/genre/movie/list')
-    res.status(200).json(data);
+    const { genres } = await fetchTMDB('/genre/movie/list');
+    res.status(200).json({ genres: Object.fromEntries(genres.map(genre => [genre.id, genre])) });
 });
 
 router.get('/discover', async (req, res) => {
     const query = new URLSearchParams(req.query);
     const { results } = await fetchTMDB('/discover/movie', query.toString());
-    const movies = Object.fromEntries(results.map(result => [result.id, result]));
-    res.status(200).json({ movies });
-});
-
-router.get('/movies/:movieId/recommendations', async (req, res) => {
-    const { movieId } = req.params;
-    const { results } = await fetchTMDB(`/movie/${movieId}/recommendations`);
-    const movies = Object.fromEntries(results.map(result => [result.id, result]));
+    const movies = Object.fromEntries(results.map(result => [result.id, extractAllowedParams(result)]));
     res.status(200).json({ movies });
 });
 
 router.get('/movies/now_playing', async (req, res) => {
     const query = new URLSearchParams(req.query);
     const { results }  = await fetchTMDB('/movie/now_playing', query.toString());
-    const movies = Object.fromEntries(results.map(result => [result.id, result]));
+    const movies = Object.fromEntries(results.map(result => [result.id, extractAllowedParams(result)]));
     res.status(200).json( {movies} )
-})
+});
+
+router.get('/movies/:movieId/recommendations', async (req, res) => {
+    const { movieId } = req.params;
+    const { results } = await fetchTMDB(`/movie/${movieId}/recommendations`);
+    const movies = Object.fromEntries(results.map(result => [result.id, extractAllowedParams(result)]));
+    res.status(200).json({ movies });
+});
 
 
 router.get('/movies/:movieId', async (req, res) => {
     const { movieId } = req.params;
-    const data = await fetchTMDB(`/movie/${movieId}`);
-    const movie = { [data.id]: data };
-    res.status(200).json(movie);
+    const movie = await fetchTMDB(`/movie/${movieId}`);
+    res.status(200).json({ [movie.id]: extractAllowedParams(movie) });
 });
-
-
 
 export default router;
