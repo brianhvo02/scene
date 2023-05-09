1a6cbe52689c60571aea84855359e75ac60549d6
Joshua Lee <josh.lee134@hotmail.com>
2023-05-08 17:48:34 -0700

    create movie store partially

--- frontend/src/store/movie.js
+++ frontend/src/store/movie.js
@@ -1,3 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 import jwtFetch from './jwt';
 
+const initialState = {}
+
+const movieSlice = createSlice({
+    name: "movie",
+    initialState,
+    reducers: {
+        receiveMovies: (state, action) => ({...state, ...action.payload.movies}),
+        // receiveMovie: (state, action) => ({...state, ...action.payload })
+    }
+});
+
+const fetchDiscoverMovies = (user) => async dispatch => {
+    const genreString = user.genreIds.join('|')
+    const res = await fetch(`/api/tmdb/discover`, genreString);
+    const { movies } = res.json()
+    dispatch(receiveMovies(movies))
+}
+
+export default movieSlice.reducer;
