91fa5032e4c0f57c69b934aecf61b926699f6a3d
Joshua Lee <josh.lee134@hotmail.com>
2023-05-11 12:04:51 -0700

    style event show page

--- frontend/src/components/Events/EventsShow/index.js
+++ frontend/src/components/Events/EventsShow/index.js
@@ -12,35 +12,37 @@ const EventShow = () => {
     const { movieId, eventId } = useParams();
 
     const dispatch = useDispatch();
-    const [rsvp, setRSVP] = useState(false);
-
+    const [going, setGoing] = useState(false);
+    const sessionUser = useSelector(state => state.session.user);
     const movie = useSelector(getMovie(movieId));
     const event = movie?.events.find(e => e._id === eventId);
     const eventDate = useMemo(() => new Date(event?.date), [event]);
     const eventCreatedDate = useMemo(() => new Date(event?.createdAt), [event]);
 
+    if(event?.attendees.includes(sessionUser.id)){
+        setGoing(true);
+    }
+
     const MOVIE_LINK = "https://image.tmdb.org/t/p/original";
 
-    const handleEventRSVPSubmit = (e) => {
-        e.preventDefault();
-        if(rsvp){
-            dispatch(addEventAttendee(movieId, eventId));
-        } else {
-            dispatch(removeEventAttendee(movieId, eventId));
-        }
+    const handleEventRSVPClick = () => {
+        dispatch(addEventAttendee(movieId, eventId));
     }
+
 
     useEffect(() => {
         if (movieId) dispatch(fetchMovie(movieId));
     }, [dispatch]);
 
     return(
+        <>
+        <img src={`${MOVIE_LINK.concat(movie?.backdropPath)}`} alt={`${movie?.title} movie backdrop`} className="background-image"/>       
+        <h1 className="event-title">{event?.title}</h1>
         <div className="event-show-page-container">
             <div className="event-show-page-left">
                 <div className="event-show-page-movie-poster">
-                    <h1>{event?.title}</h1>
                     <img src={`${MOVIE_LINK.concat(movie?.posterPath)}`} alt={`${movie?.title} movie poster`}/>
-                    <h2>{movie?.title}</h2>
+                    <h2 className="event-title-movie-title">{movie?.title}</h2>
                 </div>
                 <div className="event-show-page-description">
                     <p>Host: {event?.host.username}</p>
@@ -54,7 +56,7 @@ const EventShow = () => {
                     })}</p>
                     <p>{event?.body}</p>
                     <div>
-                        <h2>Amenities</h2>
+                        <h3>Amenities</h3>
                         <ul>
                             {
                                 event?.amenities.map((amenity, i) =>
@@ -68,7 +70,7 @@ const EventShow = () => {
             </div>
             <div className="event-show-page-right">
                 <div className="event-show-page-time-date-rsvp">
-                    <p>{eventDate.toLocaleString('en-US', {
+                    <p id="date">{eventDate.toLocaleString('en-US', {
                         weekday: 'short',
                         month: 'short',
                         day: '2-digit',
@@ -76,30 +78,32 @@ const EventShow = () => {
                         hour: '2-digit',
                         minute: '2-digit'
                     })}</p>
-                    <p>Please RSVP Below</p>
-                    <form className="event-rsvp-form" onSubmit={(e) => handleEventRSVPSubmit(e)}>
-                        <input  type="radio" name="rsvp" id="rsvp-yes" value={true}></input>
-                        <label htmlFor="rsvp-yes" onClick={() => setRSVP(true)}>I'm coming!</label>
-                        <input  type="radio" name="rsvp" id="rsvp-no" value={false}></input>
-                        <label htmlFor="rsvp-no" onClick={() => setRSVP(false)}>I'm not coming!</label>
-                        <button>RSVP!</button>
-                    </form>
-                    {rsvp ? <p>See you there!</p> : <p>Sorry you're not coming!</p>}
+                    <p>Please RSVP Below: </p>
+                    {sessionUser && movie && (
+                        <button className="rsvp-button" onClick={handleEventRSVPClick}>{ going ? "UnRSVP!" : "RSVP!"}</button>
+                        )
+                    }
+                    {going ? <p>See you there!</p> : <p>Sorry you're not coming!</p>}                
                 </div>
                 <div className="event-show-page-maps-container">
                     <h3>{event?.theater}</h3>
                     <p>Ticket Type: {event?.ticketType}</p>
                     <a href={event?.ticketUrl} target='_blank' rel='noreferrer'>Get a ticket</a>
-                    {
-                        event &&
-                        <EventMap theaters={[{
-                            name: event.theater,
-                            geo: event.coordinates
-                        }]} selected={event.theater} canSelect={false} />
-                    }
+                    <div className="map">
+                        {
+                            event &&
+                            <EventMap theaters={[{
+                                name: event.theater,
+                                geo: event.coordinates
+                            }]} selected={event.theater} canSelect={false} />
+                        }
+                    </div>
+
                 </div>
             </div>
         </div>
+        </>
+
     )
 }
 
