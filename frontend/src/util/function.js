aa1f3189b81351a310c398c46f410fd8fef12d36
Joshua Lee <josh.lee134@hotmail.com>
2023-07-06 13:55:49 -0700

    small change

--- frontend/src/util/function.js
+++ frontend/src/util/function.js
@@ -2,7 +2,7 @@ export function shuffle(array) {
     let currentIndex = array.length, randomIndex;
 
     // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
 
         // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
